539922bcb73d5b9f540127858f801fde6998dfc2
ckm960411 <a01098956117@gmail.com>
2023-10-18 02:43:07 +0900

    createLink 생성

--- src/link/link.service.ts
+++ src/link/link.service.ts
@@ -4,4 +4,8 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class LinkService {
   constructor(private prisma: PrismaService) {}
+
+  async createLink(links: { name: string; href: string }[]) {
+    return this.prisma.link.createMany({ data: links });
+  }
 }
