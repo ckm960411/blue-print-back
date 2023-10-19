e2b5394e3146c3ec82988b29489fdeee14a84851
ckm960411 <a01098956117@gmail.com>
2023-10-20 02:15:33 +0900

    update findAllTasks order by priority

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { addDays, formatISO } from 'date-fns';
+import { pipe, uniqBy, flatten } from 'lodash/fp';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateTaskReqDto } from './dto/create-task.req.dto';
 
@@ -6,8 +8,51 @@ import { CreateTaskReqDto } from './dto/create-task.req.dto';
 export class TaskService {
   constructor(private prisma: PrismaService) {}
 
-  findAllTasks() {
-    return this.prisma.task.findMany();
+  async findAllTasks() {
+    const tasks = await Promise.all([
+      this.findOnlyPriorityFiveTasks(),
+      this.findNearDeadlineTasks(),
+      this.findOnlyBookmarkedTasks(),
+      this.findAllMemosOrderByCraetedAt(),
+    ]);
+    return pipe(flatten, uniqBy('id'))(tasks);
+  }
+
+  findOnlyPriorityFiveTasks() {
+    return this.prisma.task.findMany({
+      where: { deletedAt: null, priority: 5 },
+      orderBy: { createdAt: 'asc' },
+    });
+  }
+
+  findNearDeadlineTasks() {
+    const now = new Date();
+    const twoDaysLater = addDays(now, 2);
+
+    return this.prisma.task.findMany({
+      where: {
+        deletedAt: null,
+        endAt: {
+          gte: formatISO(now),
+          lte: formatISO(twoDaysLater),
+        },
+      },
+      orderBy: { createdAt: 'asc' },
+    });
+  }
+
+  findOnlyBookmarkedTasks() {
+    return this.prisma.task.findMany({
+      where: { deletedAt: null, isBookmarked: true },
+      orderBy: { createdAt: 'asc' },
+    });
+  }
+
+  findAllMemosOrderByCraetedAt() {
+    return this.prisma.task.findMany({
+      where: { deletedAt: null },
+      orderBy: { createdAt: 'asc' },
+    });
   }
 
   createTask(createTaskReqDto: CreateTaskReqDto) {
