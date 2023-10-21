2ab9455a16f48e3bfe5484a66940e5f9794aa2e4
ckm960411 <a01098956117@gmail.com>
2023-10-22 02:03:16 +0900

    add createMilestone API

--- src/milestone/milestone.controller.ts
+++ src/milestone/milestone.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { MilestoneService } from './milestone.service';
 
 @Controller('milestone')
 export class MilestoneController {
   constructor(private readonly milestoneService: MilestoneService) {}
+
+  @Post()
+  createMilestone() {
+    return this.milestoneService.createMilestone();
+  }
 }
--- src/milestone/milestone.service.ts
+++ src/milestone/milestone.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Milestone } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class MilestoneService {
   constructor(private prisma: PrismaService) {}
+
+  async createMilestone() {
+    return this.prisma.milestone.create({
+      data: {} as Milestone,
+    });
+  }
 }
