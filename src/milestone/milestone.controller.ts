a820d21b4c27ec2f3d0dfe452d9bcba1fdf2eae3
ckm960411 <a01098956117@gmail.com>
2023-10-22 02:34:23 +0900

    add getAllMilesones API

--- src/milestone/milestone.controller.ts
+++ src/milestone/milestone.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { MilestoneService } from './milestone.service';
 
 @Controller('milestone')
 export class MilestoneController {
   constructor(private readonly milestoneService: MilestoneService) {}
+
+  @Get()
+  getAllMilestones() {
+    return this.milestoneService.findAllMilestones();
+  }
 
   @Post()
   createMilestone() {
--- src/milestone/milestone.service.ts
+++ src/milestone/milestone.service.ts
@@ -6,6 +6,18 @@ import { PrismaService } from '../prisma/prisma.service';
 export class MilestoneService {
   constructor(private prisma: PrismaService) {}
 
+  async findAllMilestones() {
+    return this.prisma.milestone.findMany({
+      where: { deletedAt: null },
+      include: {
+        tags: true,
+        links: true,
+        tasks: true,
+        memos: true,
+      },
+    });
+  }
+
   async createMilestone() {
     return this.prisma.milestone.create({
       data: {} as Milestone,
