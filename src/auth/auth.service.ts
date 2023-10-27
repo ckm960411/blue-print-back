668300279e7e1cbce633cb324d22577877e93d72
ckm960411 <a01098956117@gmail.com>
2023-10-27 22:10:20 +0900

    add login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthEntity } from './entity/auth.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async login(email: string, password: string): Promise<AuthEntity> {
+    const user = await this.prisma.user.findFirst({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException(`찾는 이메일의 계정이 없습니다`);
+    }
+
+    const isPasswordValid = user.password === password;
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('계정 정보가 일치하지 않습니다.');
+    }
+
+    return {
+      accessToken: this.jwtService.sign({ userId: user.id }),
+    };
+  }
 }
