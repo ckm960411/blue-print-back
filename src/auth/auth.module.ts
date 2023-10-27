b120bb73717bd9da6bee7003dd17d88a28879119
ckm960411 <a01098956117@gmail.com>
2023-10-27 22:43:47 +0900

    add JwtModule in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     PrismaModule,
     PassportModule,
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -19,6 +22,8 @@ import { AuthService } from './auth.service';
       }),
       inject: [ConfigService],
     }),
+    UserModule,
   ],
+  exports: [JwtModule],
 })
 export class AuthModule {}
