ec8bc5da7de42da114b1e246e7e2b9d7971f8e49
ckm960411 <a01098956117@gmail.com>
2023-10-28 04:52:15 +0900

    add console log

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8000);
+  await app.listen(8000, () => console.log('app start'));
 }
 bootstrap();
