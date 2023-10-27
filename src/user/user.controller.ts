dfe43f8f7435ce401c3075834eb3928d044046d0
ckm960411 <a01098956117@gmail.com>
2023-10-28 00:38:09 +0900

    add createUser controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,26 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
   ParseIntPipe,
+  Post,
   UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { CreateUserReqDto } from './dto/create-user.req.dto';
 import { UserEntity } from './entity/user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() createUserReqDto: CreateUserReqDto) {
+    return this.userService.createUser(createUserReqDto);
+  }
 
   @Get(':id')
   @UseGuards(JwtAuthGuard)
