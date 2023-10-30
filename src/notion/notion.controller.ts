33dfeecf9562fc60e19cf60752bc9739b3829d77
ckm960411 <a01098956117@gmail.com>
2023-10-30 20:58:54 +0900

    add getBlockChildrenById API

--- src/notion/notion.controller.ts
+++ src/notion/notion.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { OptionalIntPipe } from '../../utils/decorators/optional-int.pipe';
 import { NotionService } from './notion.service';
 
 @Controller('notion')
@@ -8,5 +9,13 @@ export class NotionController {
   @Get('pages/:id')
   async getPageById(@Param('id') page_id: string) {
     return this.notionService.getPageById(page_id);
+  }
+
+  @Get('blocks/:id/children')
+  async getBlockChildrenById(
+    @Param('id') block_id: string,
+    @Query('page_size', new OptionalIntPipe()) page_size?: number,
+  ) {
+    return this.notionService.getBlockChildrenById(block_id, page_size);
   }
 }
--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -6,4 +6,11 @@ export class NotionService {
   async getPageById(page_id: string) {
     return await notionApi.pages.retrieve({ page_id });
   }
+
+  async getBlockChildrenById(block_id: string, page_size = 100) {
+    return await notionApi.blocks.children.list({
+      block_id,
+      page_size,
+    });
+  }
 }
