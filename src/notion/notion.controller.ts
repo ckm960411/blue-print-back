d661b7343704694a50e2fefd1d4b86b0a747b90a
ckm960411 <a01098956117@gmail.com>
2023-10-30 20:55:08 +0900

    add getPageById API

--- src/notion/notion.controller.ts
+++ src/notion/notion.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { NotionService } from './notion.service';
 
 @Controller('notion')
 export class NotionController {
   constructor(private readonly notionService: NotionService) {}
+
+  @Get('pages/:id')
+  async getPageById(@Param('id') page_id: string) {
+    return this.notionService.getPageById(page_id);
+  }
 }
--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { notionApi } from './notion-client';
 
 @Injectable()
-export class NotionService {}
+export class NotionService {
+  async getPageById(page_id: string) {
+    return await notionApi.pages.retrieve({ page_id });
+  }
+}
