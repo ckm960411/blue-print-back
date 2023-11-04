38292db2861ef8e4fd63e2ddcf7cd2d850ce3636
ckm960411 <a01098956117@gmail.com>
2023-11-05 00:35:25 +0900

    feature: add createComment API

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { CreateCommentReqDto } from './dto/create-comment.req.dto';
 
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  @Post()
+  async createComment(@Body() createCommentReqDto: CreateCommentReqDto) {
+    return this.commentService.createComment(createCommentReqDto);
+  }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateCommentReqDto } from './dto/create-comment.req.dto';
 
 @Injectable()
 export class CommentService {
   constructor(private prisma: PrismaService) {}
+
+  async createComment(createCommentReqDto: CreateCommentReqDto) {
+    return this.prisma.comment.create({ data: createCommentReqDto });
+  }
 }
