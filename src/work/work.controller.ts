f0f13592549d448da563a0c2ea3342718b0433c8
ckm960411 <a01098956117@gmail.com>
2023-11-21 01:06:29 +0900

    ✨ Add: getWorkCount API

--- src/work/work.controller.ts
+++ src/work/work.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { WorkService } from './work.service';
 
 @Controller('work')
 export class WorkController {
   constructor(private readonly workService: WorkService) {}
+
+  @Get(':projectId/count')
+  async getWorkCount(
+    @Param('projectId', new ParseIntPipe()) projectId: number,
+  ) {
+    return this.workService.getWorkCount(projectId);
+  }
 }
--- src/work/work.service.ts
+++ src/work/work.service.ts
@@ -4,4 +4,18 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class WorkService {
   constructor(private prisma: PrismaService) {}
+
+  async getWorkCount(projectId: number) {
+    const milestoneCount = await this.prisma.milestone.count({
+      where: { projectId },
+    });
+    const taskCount = await this.prisma.task.count({ where: { projectId } });
+    const memoCount = await this.prisma.memo.count({ where: { projectId } });
+
+    return {
+      Milestone: milestoneCount,
+      Task: taskCount,
+      Memo: memoCount,
+    };
+  }
 }
