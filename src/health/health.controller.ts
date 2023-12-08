6e8d36431ec4590b002c703f1cbfdbe4bdc7f644
ckm960411 <a01098956117@gmail.com>
2023-12-09 05:30:13 +0900

    ✨ Add getMonthExercises API

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { OptionalIntPipe } from '../../utils/decorators/optional-int.pipe';
+import { User } from '../../utils/decorators/user.decorator';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { UserEntity } from '../user/entity/user.entity';
 import { HealthService } from './health.service';
 
 @Controller('health')
 export class HealthController {
   constructor(private readonly healthService: HealthService) {}
+
+  @Get('exercises')
+  @UseGuards(JwtAuthGuard)
+  async getMonthExercises(
+    @User() user: UserEntity,
+    @Query('year', new OptionalIntPipe()) year?: number,
+    @Query('month', new OptionalIntPipe()) month?: number,
+  ) {
+    return this.healthService.getMonthExercises(user.id, { year, month });
+  }
 }
--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { endOfMonth, getMonth, getYear, startOfMonth } from 'date-fns';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class HealthService {
   constructor(private prisma: PrismaService) {}
+
+  async getMonthExercises(
+    userId: number,
+    { year, month }: { year?: number; month?: number },
+  ) {
+    const yearToFind = year ?? getYear(new Date());
+    const monthToFind = month ? month - 1 : getMonth(new Date());
+
+    return this.prisma.userExercise.findMany({
+      where: {
+        userId,
+        exercise: {
+          date: {
+            gte: startOfMonth(new Date(yearToFind, monthToFind)),
+            lte: endOfMonth(new Date(yearToFind, monthToFind)),
+          },
+        },
+      },
+      include: {
+        exercise: true,
+      },
+    });
+  }
 }
