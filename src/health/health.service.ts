bbfb24b5bd4ed4a1efcb02a46b61bd586b7b3d57
ckm960411 <a01098956117@gmail.com>
2023-12-09 16:29:51 +0900

    ♻️ Refactor: getExercises & getMonthExercises

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,36 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { endOfMonth, getMonth, getYear, startOfMonth } from 'date-fns';
-import { PrismaService } from '../prisma/prisma.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { omit } from 'lodash';
+import { flow } from 'lodash/fp';
+import { getMonth, getYear, isDate } from 'date-fns';
+import { format, startOfMonth, endOfMonth } from 'date-fns/fp';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class HealthService {
   constructor(private prisma: PrismaService) {}
 
-  async getMonthExercises(
-    userId: number,
-    { year, month }: { year?: number; month?: number },
-  ) {
-    const yearToFind = year ?? getYear(new Date());
-    const monthToFind = month ? month - 1 : getMonth(new Date());
+  async getExercises(userId: number, from: string, to: string) {
+    if (!isDate(new Date(from)) || !isDate(new Date(to))) {
+      throw new BadRequestException('날짜 정보를 올바르게 입력해주세요');
+    }
+
+    const start = new Date(from);
+    const end = new Date(to);
 
     const userExercises = await this.prisma.userExercise.findMany({
       where: {
         userId,
-        exercise: {
-          date: {
-            gte: startOfMonth(new Date(yearToFind, monthToFind)),
-            lte: endOfMonth(new Date(yearToFind, monthToFind)),
-          },
-        },
+        exercise: { date: { gte: start, lte: end } },
       },
-      include: {
-        exercise: {
-          include: {
-            exerciseType: true,
-          },
-        },
-      },
+      include: { exercise: { include: { exerciseType: true } } },
     });
 
     // userExercises 배열에서 exercise 객체만을 추출하여 새 배열을 생성
@@ -43,5 +36,20 @@ export class HealthService {
       };
       return omit(exerciseWithType, 'exerciseType');
     });
+  }
+
+  async getMonthExercises(
+    userId: number,
+    { year, month }: { year?: number; month?: number },
+  ) {
+    const yearToFind = year ?? getYear(new Date());
+    const monthToFind = month ? month - 1 : getMonth(new Date());
+
+    const dateToFind = new Date(yearToFind, monthToFind);
+
+    const start = flow(startOfMonth, format('yyyy-MM-dd'))(dateToFind);
+    const end = flow(endOfMonth, format('yyyy-MM-dd'))(dateToFind);
+
+    return this.getExercises(userId, start, end);
   }
 }
