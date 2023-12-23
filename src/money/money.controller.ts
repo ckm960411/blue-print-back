ccaf2f74eaa3de02746733669bc7c49337c3a8b2
ckm960411 <a01098956117@gmail.com>
2023-12-23 23:37:09 +0900

    ✨ Create createMonthlyBudget API

--- src/money/money.controller.ts
+++ src/money/money.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { User } from '../../utils/decorators/user.decorator';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
+import { UserEntity } from '../user/entity/user.entity';
+import { CreateMonthlyBudgetReqDto } from './dto/create-monthly-budget.req.dto';
 import { MoneyService } from './money.service';
 
 @Controller('money')
 export class MoneyController {
   constructor(private readonly moneyService: MoneyService) {}
+
+  @Post('budget/monthly')
+  @UseGuards(JwtAuthGuard)
+  async createMonthlyBudget(
+    @User() user: UserEntity,
+    @Body() data: CreateMonthlyBudgetReqDto,
+  ) {
+    return this.moneyService.createMonthlyBudget(user.id, data);
+  }
 }
--- src/money/money.service.ts
+++ src/money/money.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateMonthlyBudgetReqDto } from './dto/create-monthly-budget.req.dto';
 
 @Injectable()
 export class MoneyService {
   constructor(private prisma: PrismaService) {}
+
+  async createMonthlyBudget(userId: number, data: CreateMonthlyBudgetReqDto) {
+    return this.prisma.monthlyBudget.create({
+      data: { userId, ...data },
+    });
+  }
 }
