e732eec2288323c7d62abab7caac9fb1e46c428c
ckm960411 <a01098956117@gmail.com>
2023-12-24 00:30:04 +0900

    ✨ Create getMonthlyBudget API

--- src/money/money.controller.ts
+++ src/money/money.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { User } from '../../utils/decorators/user.decorator';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { UserEntity } from '../user/entity/user.entity';
@@ -8,6 +8,15 @@ import { MoneyService } from './money.service';
 @Controller('money')
 export class MoneyController {
   constructor(private readonly moneyService: MoneyService) {}
+
+  @Get('budget/monthly')
+  @UseGuards(JwtAuthGuard)
+  async getMonthlyBudget(
+    @User() user: UserEntity,
+    @Query('date') date: string, // yyyy-MM-dd
+  ) {
+    return this.moneyService.getMonthlyBudget(user.id, date);
+  }
 
   @Post('budget/monthly')
   @UseGuards(JwtAuthGuard)
--- src/money/money.service.ts
+++ src/money/money.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { addMonths, getMonth, getYear, isBefore, isWeekend } from 'date-fns';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateMonthlyBudgetReqDto } from './dto/create-monthly-budget.req.dto';
 
@@ -6,9 +7,42 @@ import { CreateMonthlyBudgetReqDto } from './dto/create-monthly-budget.req.dto';
 export class MoneyService {
   constructor(private prisma: PrismaService) {}
 
+  /**
+   * @param userId userId
+   * @param date yyyy-MM-dd
+   */
+  async getMonthlyBudget(userId: number, date: string) {
+    const dateToFind = new Date(date);
+    const year = getYear(dateToFind);
+    const month = getMonth(dateToFind) + 1;
+
+    const start = this.findMonthlyBudgetStartDay(year, month);
+    const monthToFind = isBefore(dateToFind, start)
+      ? getMonth(addMonths(dateToFind, -1)) + 1
+      : month;
+
+    return this.prisma.monthlyBudget.findFirst({
+      where: { userId, month: monthToFind },
+    });
+  }
+
   async createMonthlyBudget(userId: number, data: CreateMonthlyBudgetReqDto) {
     return this.prisma.monthlyBudget.create({
       data: { userId, ...data },
     });
+  }
+
+  findMonthlyBudgetStartDay(year: number, month: number) {
+    const is15DayWeekend = isWeekend(new Date(year, month - 1, 15));
+
+    // 15일이 평일이라면 15일 반환
+    if (!is15DayWeekend) return new Date(year, month - 1, 15);
+
+    // 15일이 주말이라면 14일을, 14일도 주말이라면 13일 반환
+    const is14DayWeekend = isWeekend(new Date(year, month - 1, 14));
+    if (is14DayWeekend) {
+      return new Date(year, month - 1, 13);
+    }
+    return new Date(year, month - 1, 14);
   }
 }
