341a0d04c8da7ae89e432581b38437838fffb3f6
ckm960411 <a01098956117@gmail.com>
2023-12-30 06:18:33 +0900

    ♻️ Refactor: create-expenditure.req.dto.ts

--- src/money/dto/create-expenditure.req.dto.ts
+++ src/money/dto/create-expenditure.req.dto.ts
@@ -2,6 +2,9 @@ export interface CreateExpenditureReqDto {
   type: 'INCOME' | 'SPENDING';
   year: number;
   month: number;
+  date: number;
+  hour?: number;
+  minute?: number;
   content: string;
   price: number;
   monthlyBudgetCategoryId?: number;
